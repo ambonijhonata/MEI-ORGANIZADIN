a8d3ae79ddd5f20659b553d7ace972fd19591ab1
Jhonata Amboni <jhonata01amboni@gmail.com>
2026-06-05 21:58:41 -0300

    Refine application bootstrap PMD cleanup

--- src/main/java/com/api/MeiOrganizadinApplication.java
+++ src/main/java/com/api/MeiOrganizadinApplication.java
@@ -3,15 +3,28 @@ package com.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SuppressWarnings("PMD.UseUtilityClass")
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableAsync
-public class MeiOrganizadinApplication {
+public class MeiOrganizadinApplication extends SpringBootServletInitializer {
+
+    private final Class<?> applicationSource;
+
+    public MeiOrganizadinApplication() {
+        super();
+        this.applicationSource = MeiOrganizadinApplication.class;
+    }
 
     public static void main(final String[] args) {
         SpringApplication.run(MeiOrganizadinApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(this.applicationSource);
     }
 }
