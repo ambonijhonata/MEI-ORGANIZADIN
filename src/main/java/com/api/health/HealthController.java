66ad515ff7fb9117d8fef41655a590079aec57c7
Jhonata Amboni <jhonata01amboni@gmail.com>
2026-06-05 22:17:05 -0300

    Refine health controller PMD cleanup

--- src/main/java/com/api/health/HealthController.java
+++ src/main/java/com/api/health/HealthController.java
@@ -4,12 +4,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SuppressWarnings("PMD.AtLeastOneConstructor")
 @RestController
 public class HealthController {
 
+    private final ResponseEntity<Boolean> healthyResponse;
+
+    public HealthController() {
+        this.healthyResponse = ResponseEntity.ok(Boolean.TRUE);
+    }
+
     @GetMapping("/healthz")
     public ResponseEntity<Boolean> healthz() {
-        return ResponseEntity.ok(Boolean.TRUE);
+        return this.healthyResponse;
     }
 }
