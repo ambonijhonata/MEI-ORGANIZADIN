35c2581ac4c2eadc599792949bf3cb93ec3bd25d
Jhonata Amboni <jhonata01amboni@gmail.com>
2026-08-02 17:35:21 -0300

    Run integration tests with security filters

--- src/test/java/com/api/calendar/ManualAppointmentIntegrationTest.java
+++ src/test/java/com/api/calendar/ManualAppointmentIntegrationTest.java
@@ -30,7 +30,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMockMvc
 class ManualAppointmentIntegrationTest extends IntegrationTestBase {
 
     @Autowired
